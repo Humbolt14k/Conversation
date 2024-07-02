e48a685b5d18a28970d9c70eea8387ebffa73846
Chris Palmer <chris@theobuild.com>
2024-07-02 09:41:59 -0500

    Added new question route as a GET operation

--- function_app.py
+++ function_app.py
@@ -5,21 +5,36 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="main")
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP POST trigger function called main is processing a request')
 
-    name = req.params.get('name')
-    if not name:
+    name_parameter = req.params.get('name')
+    if not name_parameter:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name_parameter = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if name_parameter:
+        return func.HttpResponse(f"Hello, {name_parameter}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully",
              status_code=200
         )
+    
+@app.route(route="question")
+def question(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP GET trigger function called question is processing a request')
+
+    text_parameter = req.params.get('text')
+    if text_parameter:
+        if text_parameter.lower() == 'hi':
+            return func.HttpResponse("How you doing?")
+        elif text_parameter.lower() == 'bye':
+            return func.HttpResponse("So long")
+        else:
+            return func.HttpResponse("Did not compute the text that was provided")
+    else:
+        return func.HttpResponse("Please provide a text parameter")
